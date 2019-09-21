76194e7c15ace98303e4c556ac15f4abad90e6b3
dariushine <frederic_788@hotmail.com>
2019-09-20 20:15:37 -0400

    Added comments (Linguo)

--- project1/script.js
+++ project1/script.js
@@ -1,7 +1,9 @@
+// SmoothScroll setup
 let scroll = new SmoothScroll('a[href*="#"]', {
   speed: 900
 });
 
+// Function for the navbar button
 function dropdown() {
   const x = document.getElementById('nav-bar');
   if (x.className === '') {
