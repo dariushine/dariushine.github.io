d64b7b4e1630c74ce22fcb11816733d25635cf16
dariushine <frederic_788@hotmail.com>
2020-02-17 17:00:26 -0400

    Changed color, portfolio section,  and more

--- js/script.js
+++ js/script.js
@@ -4,31 +4,31 @@ var scroll = new SmoothScroll('a[href*="#"]', {
 });
 
 $(document).ready(function () {
-      $('.slider-showcase').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-        asNavFor: '.slider-nav,.slider-description'
-      });
 
-      $('.slider-description').slick({
-        draggable: false,
-        fade: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-        adaptiveHeight: true,
-        asNavFor: '.slider-nav,.slider-showcase'
-      });
 
       $('.slider-nav').slick({
         slidesToShow: 3,
         dots: true,
         centerMode: true,
-        arrows: false,
         centerPadding: '0',
         focusOnSelect: true,
         swipeToSlide: true,
-        asNavFor: '.slider-showcase,.slider-description'
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 2,
+              slidesToSroll: 1,
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              arrows: false,
+              slidesToShow: 1,
+              slidesToSroll: 1,
+            }
+          }
+        ]
       });
     });
