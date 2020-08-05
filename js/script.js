f391fe83a4a491ee55ff4b69dbdf2754b9aa6052
dariushine <frederic_788@hotmail.com>
2020-08-04 20:37:16 -0400

    Changed top section (header and nav)

--- js/script.js
+++ js/script.js
@@ -1,21 +1,31 @@
-var smoothscroll = new SmoothScroll('a[href*="#"]', {
-  header: 'nav',
-  speed: 900
-});
+$(document).ready(function () {
+  var smoothscroll = new SmoothScroll('a[href*="#"]', {
+    header: "nav",
+    speed: 900,
+  });
+  //>=, not <=
+  if ($(window).scrollTop() >= 60 || $(window).width() < 576) {
+    //clearHeader, not clearheader - caps H
+    $(".navbar").addClass("bg-primary");
+  }
 
-$(document).ready(function() {
-
-
-  $(window).scroll(function() {    
+  $(window).scroll(function () {
+    if ($(window).width() >= 576) {
       var scroll = $(window).scrollTop();
-  
-       //>=, not <=
+
+      //>=, not <=
       if (scroll >= 60) {
-          //clearHeader, not clearheader - caps H
-          $(".navbar").addClass("bg-primary");
+        //clearHeader, not clearheader - caps H
+        $(".navbar").addClass("bg-primary");
       } else {
         $(".navbar").removeClass("bg-primary");
       }
+    }
   });
 
-})
+  $(window).resize(function () {
+    if ($(window).width() < 576) $(".navbar").addClass("bg-primary");
+    else if ($(window).width() >= 576 && $(window).scrollTop() < 60)
+      $(".navbar").removeClass("bg-primary");
+  });
+});
