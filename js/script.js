4d33fab0ea916099588e35a71016200354c6466f
dariushine <frederic_788@hotmail.com>
2020-08-07 15:16:55 -0400

    Commented js, changed skills whitespace

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
 $(document).ready(function () {
+  // Starts smooth scroll and fixes problem with links and nav bar
   var smoothscroll = new SmoothScroll('a[href*="#"]', {
     header: "nav",
     speed: 900,
   });
-  //>=, not <=
+
+  // Sets the nav bar background when the page starts (depending on factors)
   if ($(window).scrollTop() >= 60 || $(window).width() < 576) {
-    //clearHeader, not clearheader - caps H
     $(".navbar").addClass("bg-primary");
   }
 
+  // Toggles the nav bar background when the screen is big enough and the position is adequate
   $(window).scroll(function () {
     if ($(window).width() >= 576) {
       var scroll = $(window).scrollTop();
 
-      //>=, not <=
       if (scroll >= 60) {
-        //clearHeader, not clearheader - caps H
         $(".navbar").addClass("bg-primary");
       } else {
         $(".navbar").removeClass("bg-primary");
@@ -23,6 +23,7 @@ $(document).ready(function () {
     }
   });
 
+  // Makes the nav bar background always visible when the window width is small
   $(window).resize(function () {
     if ($(window).width() < 576) $(".navbar").addClass("bg-primary");
     else if ($(window).width() >= 576 && $(window).scrollTop() < 60)
